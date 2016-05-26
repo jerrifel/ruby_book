8c1c937957b4ee318dd7a4927056db56ac94d125
AdventuresWithJerricko <adventureswithjerricko@gmail.com>
2016-05-26 17:45:44 -0400

    first commit

---
+++ methods/exercise_4.rb
@@ -0,0 +1,12 @@
+#exercise_4.rb
+#What will the following code print to the screen?
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+#-> it will not print anything
