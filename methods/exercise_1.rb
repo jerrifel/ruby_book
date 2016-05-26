4c328c2a2458aee1bb74440a136c0dcbea4ff408
AdventuresWithJerricko <adventureswithjerricko@gmail.com>
2016-05-26 13:37:45 -0400

    first commit 1.0

---
+++ methods/exercise_1.rb
@@ -0,0 +1,10 @@
+# exercise_1.rb
+# Print a greeting message. Should contain a 
+# method called greeting and takes a name 
+# parameter and returns a string.
+
+def greeting(name)
+  puts "Hello, #{name}. Nice to meet you."
+end
+
+greeting("Juja")
