cec5f7b56fb4b6ac2971550137484480376d1d0c
AdventuresWithJerricko <adventureswithjerricko@gmail.com>
2016-05-26 14:36:37 -0400

    first commit

---
+++ methods/exercise_3.rb
@@ -0,0 +1,10 @@
+#exercise_3.rb
+#Write a program that includes a method called
+#multiply that takes two arguments and returns
+#the product of the two numbers.
+
+def multiply(a, b)
+  return a * b
+end
+
+puts multiply(3, 25)
